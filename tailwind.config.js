1e0a4c85417c1d180fdbc0a5acaae3673fa3efd0
Johno <john@mount-media-online.com>
2025-05-19 11:09:51 +0100

    init..push

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          brandBlue: "#3b82f6", // Edelman blue
+          brandTeal: "#004d6e", // Dark teal
+          brandLightTeal: "#1976d2", // Light teal
+        },
+        fontFamily: {
+          sans: ['Arial', 'Helvetica', 'Roboto', 'sans-serif'], // Clean sans-serif fonts
+        },
+      },
+    },
+    plugins: [],
+  };
+  
+  
+  post.css config
+  
+  export default {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    };
